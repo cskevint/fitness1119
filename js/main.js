26f1e4ecf92a20d032a34bbc664471081e5873d1
Kevin Trotter <cskevint@gmail.com>
2015-09-25 21:09:13 -0700

    cleanup jcarousel

--- js/main.js
+++ js/main.js
@@ -1,38 +1,3 @@
-//$(function() {
-//  $('.jcarousel')
-//    .jcarousel({
-//      wrap:'circular'
-//    })
-//    .jcarouselAutoscroll({
-//      interval: 3000,
-//      target: '+=1',
-//      autostart: true
-//    });
-//
-//  $('.jcarousel-prev')
-//    .on('jcarouselcontrol:active', function() {
-//      $(this).removeClass('inactive');
-//    })
-//    .on('jcarouselcontrol:inactive', function() {
-//      $(this).addClass('inactive');
-//    })
-//    .jcarouselControl({
-//      target: '-=1'
-//    });
-//
-//  $('.jcarousel-next')
-//    .on('jcarouselcontrol:active', function() {
-//      $(this).removeClass('inactive');
-//    })
-//    .on('jcarouselcontrol:inactive', function() {
-//      $(this).addClass('inactive');
-//    })
-//    .jcarouselControl({
-//      target: '+=1'
-//    });
-//
-//});
-
 $(document).ready(function(){
   $('.carousel').slick({
     lazyLoad: 'ondemand',
