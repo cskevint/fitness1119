13f442dc0191bc3f71857e2ef2805dd99b26e876
Kevin Trotter <cskevint@gmail.com>
2015-09-25 20:56:57 -0700

    integrated slick

--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
-$(function() {
-  $('.jcarousel')
-    .jcarousel({
-      wrap:'circular'
-    })
-    .jcarouselAutoscroll({
-      interval: 3000,
-      target: '+=1',
-      autostart: true
-    });
+//$(function() {
+//  $('.jcarousel')
+//    .jcarousel({
+//      wrap:'circular'
+//    })
+//    .jcarouselAutoscroll({
+//      interval: 3000,
+//      target: '+=1',
+//      autostart: true
+//    });
+//
+//  $('.jcarousel-prev')
+//    .on('jcarouselcontrol:active', function() {
+//      $(this).removeClass('inactive');
+//    })
+//    .on('jcarouselcontrol:inactive', function() {
+//      $(this).addClass('inactive');
+//    })
+//    .jcarouselControl({
+//      target: '-=1'
+//    });
+//
+//  $('.jcarousel-next')
+//    .on('jcarouselcontrol:active', function() {
+//      $(this).removeClass('inactive');
+//    })
+//    .on('jcarouselcontrol:inactive', function() {
+//      $(this).addClass('inactive');
+//    })
+//    .jcarouselControl({
+//      target: '+=1'
+//    });
+//
+//});
 
-  $('.jcarousel-prev')
-    .on('jcarouselcontrol:active', function() {
-      $(this).removeClass('inactive');
-    })
-    .on('jcarouselcontrol:inactive', function() {
-      $(this).addClass('inactive');
-    })
-    .jcarouselControl({
-      target: '-=1'
-    });
-
-  $('.jcarousel-next')
-    .on('jcarouselcontrol:active', function() {
-      $(this).removeClass('inactive');
-    })
-    .on('jcarouselcontrol:inactive', function() {
-      $(this).addClass('inactive');
-    })
-    .jcarouselControl({
-      target: '+=1'
-    });
-
+$(document).ready(function(){
+  $('.carousel').slick({
+    lazyLoad: 'ondemand',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    dots: false
+  });
 });
