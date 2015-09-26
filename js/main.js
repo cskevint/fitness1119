08c2f922af09e74aae85ce59f250f1a5c713d8a7
Kevin Trotter <cskevint@gmail.com>
2015-09-25 22:05:35 -0700

    added more links

--- js/main.js
+++ js/main.js
@@ -16,4 +16,30 @@ $(document).ready(function(){
     $('.nav-bar').removeClass("nav-list");
   });
 
+  $(".truncate").each(function(i, el){
+    var pTags = $(el).find("p");
+    function toggle(show) {
+      pTags.each(function(i, p){
+        if(show) {
+          $(p).show();
+        } else {
+          $(p).hide();
+        }
+      });
+    }
+
+    if(pTags.length > 1) {
+      var first = pTags.splice(0, 1);
+      var more = $("<a href='#'>More...</a>");
+      more.insertAfter(first);
+      more.click(function(e){
+        toggle(true);
+        more.remove();
+        return false;
+      });
+      toggle(false);
+    }
+
+  });
+
 });
